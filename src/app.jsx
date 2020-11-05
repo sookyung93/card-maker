e8e165bb913ce5e801c0203de549d996c3b6ad0c
sookyung93 <i.sookyung93@gmail.com>
2020-11-05 17:49:01 +0900

    add loing page section & modify firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './app.module.css';
+import Login from './components/login/login';
 
-function App() {
-  return <></>;
+function App({ authService }) {
+  return <Login authService={authService} />;
 }
 
 export default App;
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import firebaseApp from './firebase';
 
 class AuthService {
   login(providerName) {

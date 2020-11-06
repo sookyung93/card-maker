2f9a3756ea28fb7836cb8501d60014f68d55f1a6
sookyung93 <i.sookyung93@gmail.com>
2020-11-06 15:25:43 +0900

    add logout button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = () => {
+const Header = ({ onLogout = true }) => {
   return (
     <header className={styles.header}>
+      {onLogout && <button className={styles.logout}>Logout</button>}
       <img className={styles.logo} src="/images/logo.png" alt="" />
       <h1 className={styles.title}>Business Card Maker</h1>
     </header>

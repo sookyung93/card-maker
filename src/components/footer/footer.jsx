de54d4e180ce4019de2cd42863df2082245947f7
sookyung93 <i.sookyung93@gmail.com>
2020-12-01 17:18:11 +0900

    add scroll and modify footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import styles from './footer.module.css';
 const Footer = (props) => {
   return (
     <section className={styles.footer}>
-      <h1>SOOKYUNG</h1>
+      <h1>CardMaker by SOOKYUNG</h1>
     </section>
   );
 };
